e3e3ba4d5d040b9fec41e1a19599657940c4becc
shlthapa <shlokthapa101@gmail.com>
2025-10-22 13:15:58 -0400

    final commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function calculateFactorial() {
-  const input = document.getElementById('inputNumber').value;
-  let result = 1;
+  const input = document.getElementById("numberInput").value;
+  const output = document.getElementById("output");
 
+  let result = 1;
+  
   for (let i = 1; i <= input; i++) {
     result *= i;
   }
 
-  document.getElementById('output').innerText = `${input}! = ${result}`;
+  output.innerHTML = `${input}! = ${result}`;
 }
